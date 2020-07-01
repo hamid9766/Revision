c82aea1da14c6c5f2a114a6f7040be2760403622
hamid9766 <40504624+hamid9766@users.noreply.github.com>
2020-07-01 09:34:38 +0530

    Created hotel management system (Practising inheritance)

---
+++ src/hotelManagementSystem/Nurse.java
@@ -0,0 +1,9 @@
+package hotelManagementSystem;
+
+public class Nurse extends Staff {
+    private long nurseId;
+    public long getNurseId(){return nurseId;}
+    public void setNurseId(long nurseId){
+        this.nurseId = nurseId;
+    }
+}
